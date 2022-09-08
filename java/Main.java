e95a59cac8bd180bc58aa80cbce18bcf9acf70fa
roumodip <roumodipdas456@gmail.com>
2022-09-08 23:29:36 +0530

    java

---
+++ java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        for(num = 1; num<=7; num++);
+        System.out.print(num);
+    }
+}
---
+++ java/ii.java
@@ -0,0 +1,7 @@
+public class  ii {
+    public static void main(String[] args) {
+        int num;
+        for(num = 1; num<=7; num++);
+        System.out.print(num);
+    }
+}
