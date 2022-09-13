a089e6abbcf3c972143b4da8e5a560be28ada5bc
roumodip <roumodipdas456@gmail.com>
2022-09-13 22:35:51 +0530

    java

---
+++ java/calculate2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class calculate2{
+void add(int x,int y){
+int add = x+y;
+System.out.println("Sum is :"+add);
+}
+void sub(int x,int y){
+int sub = x-y;
+System.out.println("Sub is :"+sub);
+}
+void mul(int x,int y){
+int mul = x*y;
+System.out.println("multi is :"+mul);
+}
+void div(int x,int y){
+int div = x/y;
+System.out.println("div is :"+div);
+}
+void show(){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the first number ");
+int a1 = sc.nextInt();
+System.out.println("Enter the second number ");
+int a2 = sc.nextInt();
+add(a1,a2);
+sub(a1,a2);
+mul(a1,a2);
+div(a1,a2);
+}
+public static void main(String[] args){
+calculate2 ob = new calculate2();
+ob.show();
+}
+}
---
+++ java/ckeckprime.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class hehe{
+void checkPrime(){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+boolean flag = false;
+for(int i=2;i<=n/2;++i)
+{
+if(n%2==0)
+{
+flag = true;
+break;
+}
+}
+if(! flag)
+{
+System.out.println(n+" is a prime number");
+}
+else
+{
+System.out.println(n+" is not a prime number");
+}
+}
+}
+class checkprime{
+public static void main(String[] args){
+hehe h=new hehe();
+h.checkPrime();
+}
+}
